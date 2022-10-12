cc6a4c11961750d7b7b629c9907bc05f5a0a58c1
notnvm <auraz616@mail.ru>
2022-10-12 17:34:32 +0300

    reworked numerical method and added second task

--- numerical methods _lab/method.h
+++ numerical methods _lab/method.h
@@ -2,6 +2,9 @@
 
 #include <vector>
 #include <cmath>
+
+#include "main_window.h"
+
 namespace method {
 	class task {
 	public:
@@ -11,15 +14,30 @@ namespace method {
 		virtual ~task() {}
 
 		void clear_data() {
-			h = 0;
 			dots.clear();
 			dots.resize(0);
 		};
-		virtual double problem(double) = 0;
-		virtual std::vector<double> find_true_solution() = 0;
-		virtual std::vector<double> find_solution() = 0;
 
-	private:
+		virtual double problem(double, double) = 0;
+
+		std::vector<double> find_solution() {
+			double e_u = problem(0,0), e_v = 1, e_x = problem(0,0);
+			dots.push_back(e_v);
+			size_t counter = 0;
+			double k_1, k_2, k_3, k_4;
+
+			for (double x_curr = x_min; x_curr <= x_max; x_curr += h) {
+				k_1 = problem(dots[counter], 0);
+				k_2 = problem(dots[counter] + h / 2 * k_1, 0);
+				k_3 = problem(dots[counter] + h / 2 * k_2, 0);
+				k_4 = problem(dots[counter] + h * k_3, 0);
+				++counter;
+				dots.push_back(dots[counter - 1] + h / 6 * (k_1 + 2 * k_2 + 2 * k_3 + k_4));
+			}
+			return dots;
+		}
+
+	protected:
 		std::vector<double> dots;
 		double h;
 		double x_min;
@@ -30,63 +48,44 @@ namespace method {
 	public:
 
 		test_task(double h_ = 0.1, double x_min_ = 0, double x_max_ = 1)
-			: h(h_), x_min(x_min_), x_max(x_max_) {}
+			: task(h_, x_min_, x_max_) {}
 
-		double problem(double u) override {
+		double problem(double u, double x) override {
 			return (-1) * 5 / 2 * u;
 		}
 
-		std::vector<double> find_true_solution() override {
+		std::vector<double> find_true_solution() {
 			double x_curr = x_min;
-			dots.reserve(10);
 			double u = 0;
 			while (x_curr <= x_max) {
 				u = std::pow(std::exp(1.0), -5 / 2 * x_curr);
-				dots.push_back(u);
+				true_dots.push_back(u);
 				x_curr += h;
 			}
-			return dots;
-		};
-
-		std::vector<double> find_solution() override { 
-			double e_u = problem(0), e_v = 1, e_x = problem(0);
-			double x_curr = x_min;
-			clear_data();
-			dots.resize(12);
-			dots[0] = e_v;
-			size_t counter = 0;
-			double k_1, k_2, k_3, k_4;
-			while (x_curr <= x_max) {
-				k_1 = problem(dots[counter]);
-				k_2 = problem(dots[counter] + h / 2 * k_1);
-				k_3 = problem(dots[counter] + h / 2 * k_2);
-				k_4 = problem(dots[counter] + h * k_3);
-				++counter;
-				dots[counter] = dots[counter - 1] + h / 6 * (k_1 + 2 * k_2 + 2 * k_3 + k_4);
-				x_curr += h;
-			}
-
-			return dots; 
+			return true_dots;
 		};
 
 	private:
-		std::vector<double> dots;
-		double h;
-		double x_min;
-		double x_max;
+		std::vector<double> true_dots;
 	};
 
 	class first_task : public task {
-		std::vector<double> find_solution() override { return std::vector<double>(10, 0); };
+	public:
+
+		first_task(double h_ = 0.1, double x_min_ = 0, double x_max_ = 1)
+			: task(h_, x_min_, x_max_) {}
+
+		double problem(double u, double x) override {
+			return log(x + 1) / (std::pow(x, 2) + 1) * std::pow(u, 2) + u - std::pow(u, 3) * sin(10 * x);
+		}
 	};
 
 	class second_task : public task {
-		std::vector<double> find_solution() override { return std::vector<double>(10, 0); };
-	};
 
-	/*void task::clear_data() {
-		h = 0;
-		dots.clear();
-		dots.resize(0);
-	}*/
+		second_task(double h_ = 0.1, double x_min_ = 0, double x_max_ = 1)
+			: task(h_, x_min_, x_max_) {}
+
+		double problem(double u, double x) override {
+		}
+	};
 }
